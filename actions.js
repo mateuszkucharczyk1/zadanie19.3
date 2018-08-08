b22aa907e8acb87e911ecb46d83be77b8e3b6569
Mateusz Kucharczyk <mateusz_kucharczyk@yahoo.com>
2018-08-08 10:24:44 +0200

    bug fixes

--- actions.js
+++ actions.js
@@ -1,101 +1,46 @@
-import uuid from uuid;
+import uuid from 'uuid';
 
-const ADD_COMMENT = 'ADD_COMMENT';
+export const ADD_COMMENT = 'ADD_COMMENT';
+export const REMOVE_COMMENT = 'REMOVE_COMMENT';
+export const EDIT_COMMENT = 'EDIT_COMMENT';
+export const THUMB_UP_COMMENT = 'THUMB_UP_COMMENT';
+export const THUMB_DOWN_COMMENT = 'THUMB_DOWN_COMMENT';
 
-{
-  type: ADD_COMMENT,
-  text: 'My first comment'
+export function addComment(text) {
+    return {
+        type: ADD_COMMENT,
+        text,
+        id: uuid.v4()
+    }
 }
 
-const REMOVE_COMMENT = 'REMOVE_COMMENT';
-
-{
-  type: REMOVE_COMMENT,
-  id: 15
+export function removeComment(id) {
+    return {
+        type: REMOVE_COMMENT,
+        id: uuid.v4()
+    }
 }
 
-const EDIT_COMMENT = 'EDIT_COMMENT';
-
-{
-  type: EDIT_COMMENT,
-  id: 15,
-  text: 'edited text of the comment'
+export function editComment(id, text) {
+    return {
+        type: EDIT_COMMENT,
+        text,
+        id: uuid.v4()
+    }
 }
 
-const THUMB_UP_COMMENT = 'THUMB_UP_COMMENT';
-
-{
-  type: THUMB_UP_COMMENT,
-  value: +1,
-  id: 10
+export function thumbUpComment(id, vote) {
+    return {
+        type: THUMB_UP_COMMENT,
+        id: uuid.v4(),
+        vote: vote
+    }
 }
 
-const THUMB_DOWN_COMMENT = 'THUMB_DOWN_COMMENT';
-
-{
-  type: THUMB_DOWN_COMMENT,
-  value: -1,
-  id: 15
+export function thumbDownComment(id, vote) {
+    return {
+        type: THUMB_DOWN_COMMENT,
+        id: uuid.v4(),
+        vote: vote
+    }
 }
-
-
-
-function addComment(text) {
-  return {
-    type: ADD_COMMENT,
-    text,
-    id: uuid.v4()
-  }
-}
-
-function removeComment(id) {
-  return {
-    type: REMOVE_COMMENT,
-    id: comment.id
-  }
-}
-
-function editComment(id, text) {
-  return {
-    type: EDIT_COMMENT,
-    text,
-    id: comment.id
-  }
-}
-
-function thumbUpComment(value) {
-  return {
-    type: THUMB_UP_COMMENT.
-    value: +1,
-    id: uuid.v4()
-  }
-}
-
-function thumbDownComment(value) {
-  return {
-    type: THUMB_DOWN_COMMENT,
-    value: -1,
-    id: uuid.v4()
-  }
-}
-
-dispatch(addComment('new comment!'));
-dispatch(addComment('next new comment!'));
-
-
-const boundAddComment = text => dispatch(addComment(text));
-
-boundAddComment('new comment!');
-boundAddComment('next new comment!');
-
-const boundRemoveComment = id => dispatch(removeComment(id));
-boundRemoveComment(10);
-
-const boundEditComment = (id, text) => dispatch(editComment(id, text));
-boundEditComment(10, 'this is edited comment');
-
-const boundThumbUpComment = value => disptach(thumbUpComment(value));
-boundThumbUpComment(+1);
-
-const boundThumbDownComment = value => disptach(thumbDownComment(value));
-boundThumbDownComment(-1);
